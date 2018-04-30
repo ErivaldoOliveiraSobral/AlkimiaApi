e143c0e7e51fa4f1a6696dbde5d4e1ade428e849
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-30 08:43:15 -0300

    JSON_UNESCAPED_UNICODE para não quebrar acentuação

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    header('Content-Type: application/json; charset=utf-8');
     
-    include_once('model/Produto.php');
+    include_once('model/Pedido.php');
 
     //conexao
     $server = "bdalkimia.mysql.dbaas.com.br";
@@ -19,22 +20,61 @@
 	* 
 	*/
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    	$selectQuery = "select * from wp_woocommerce_order_items where order_item_type=\"line_item\"";
 
-		$resposta = mysqli_query($conexao, $selectQuery);
+        // Query SELECT de todos os pedidos
+        $selectQuery = "SELECT 
+                            pedido.id as id,
+                            pedido.id_pedido as id_pedido,
+                            produto.nome as nome_produto,
+                            produto.peso peso_produto,
+                            usuario.nome as nome_usuario,
+                            endereco.end_linha_1 as endereco_usuario, 
+                            endereco.end_linha_2 as bairro_usuario,
+                            endereco.cidade,
+                            endereco.cep,
+                            endereco.pais,
+                            endereco.estado,
+                            usuario.telefone,
+                            usuario.email,
+                            produto.preco,
+                            status.status as status_pedido
+                        FROM ws_Pedido AS pedido
+                        JOIN ws_Produto as produto on produto.id = pedido.id_produto
+                        JOIN ws_Usuario as usuario on usuario.id = pedido.id_usuario
+                        JOIN ws_Endereco as endereco on endereco.id_usuario = usuario.id
+                        JOIN ws_Status as status on status.id = pedido.id_status;";
+
+        $resposta = mysqli_query($conexao, $selectQuery);
+
   	  	$dados = array();
     	while ($dados_array = mysqli_fetch_assoc($resposta)) {
-    		$produto = new Produto();
-	    	$produto->order_item_id = $dados_array['order_item_id'];
-   			$produto->order_item_name = $dados_array['order_item_name'];
-	    	$produto->order_item_type = $dados_array['order_item_type'];
-	    	$produto->order_id = $dados_array['order_id'];
-	    	array_push($dados, $produto);
+    		$pedido = new Pedido();
+
+            $pedido->id = $dados_array['id'];
+            $pedido->numeroPedido = $dados_array['id_pedido'];
+            $pedido->produto = $dados_array['nome_produto'];
+            $pedido->peso = $dados_array['peso_produto'];
+            $pedido->nomeUsuario = $dados_array['nome_usuario'];
+            $pedido->endereco = $dados_array['endereco_usuario'];
+            $pedido->bairro = $dados_array['bairro_usuario'];
+            $pedido->cidade = $dados_array['cidade'];
+            $pedido->cep = $dados_array['cep'];
+            $pedido->pais = $dados_array['pais'];
+            $pedido->estado = $dados_array['estado'];
+            $pedido->telefone = $dados_array['telefone'];
+            $pedido->email = $dados_array['email'];
+            $pedido->preco = $dados_array['preco'];
+            $pedido->status = $dados_array['status_pedido'];
+
+
+	    	array_push($dados, $pedido);
     	}
-    	echo json_encode($dados);
-    	//echo count($dados);
+    	//echo json_encode($dados);
+        echo json_encode($dados, JSON_UNESCAPED_UNICODE); // Para não quebrar acentuação
     }
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    	// TODO: 
+    	// TODO: Falta implementar inserção
+        //json_decode($json, false, 512, JSON_UNESCAPED_UNICODE)
     }
 ?>
---
+++ model/Pedido.php
@@ -0,0 +1,22 @@
+<?php
+	
+class Pedido 
+	{
+		public $id;
+		public $numeroPedido;
+		public $produto;
+		public $peso;
+		public $nomeUsuario;
+		public $endereco;
+		public $bairro;
+		public $cidade;
+		public $cep;
+		public $pais;
+		public $estado;
+		public $telefone;
+		public $email;
+		public $preco;
+		public $status;
+	}
+
+?>
